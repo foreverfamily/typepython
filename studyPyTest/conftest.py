a7b097d4b99adf71b8d9663ad373aa3a9344184c
gonghuihui55 <yulu@dianjia.io>
2017-12-15 18:16:38 +0800

    update conftest.py

--- studyPyTest/conftest.py
+++ studyPyTest/conftest.py
@@ -7,6 +7,7 @@ import pytest
 
 # content of conftest.py
 
+"""
 @pytest.fixture(scope='session')
 def image_file(tmpdir_factory):
     img = compute_expensive_image()
@@ -18,3 +19,16 @@ def image_file(tmpdir_factory):
 def test_histogram(image_file):
     img = load_image(image_file)
     # compute and test histogram
+"""
+
+def test_func_fact():
+    print("fast")
+
+@pytest.mark.skipif(not pytest.config.getoption("--runslow"))
+def test_func_slow_1():
+    print("skip slow")
+
+@pytest.mark.xfail(not pytest.config.getoption("--runslow"))
+def test_func_slow_2():
+    print("xfail slow")
+
