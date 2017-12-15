acaf0dfc5aacc6117ab5a11c3a4a68fd36a6873a
gonghuihui55 <yulu@dianjia.io>
2017-12-15 18:16:17 +0800

    insert

---
+++ studyPyTest/test_main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/15 12:58
+# @Author  : yulu
+# @File    : test_main
+
+import pytest
+
+def test_main():
+    assert 5 != 5
+
+if __name__ == '__main__':
+    pytest.main("C:/studyPy/typepython/studyPyTest/")
---
+++ studyPyTest/test_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/15 12:58
+# @Author  : yulu
+# @File    : test_simple
